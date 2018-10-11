8a91b68211eb832531a792aa1ede2b34483e059e
aratoval <chy.pawel@gmail.com>
2018-10-11 10:32:21 +0200

    I translated the game into English

--- computer_guess_the_number.py
+++ computer_guess_the_number.py
@@ -1,23 +1,23 @@
 def game():
 
-    print("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach")
+    print("Think of a number from 0 to 1000, and I will guess it in max. 10 attempts!")
     answer = ""
 
     mins = 0
     maks = 1000
     counter = 0
-    answer_list = ["zgadłeś", "za dużo", "za mało"]
+    answer_list = ["you guessed", "less", "more"]
 
-    while answer != "zgadłeś":
+    while answer != answer_list[0]:
 
         counter += 1
         guess = int((maks - mins) / 2) + mins
-        print("Lista komend {}".format(answer_list))
-        print("Próba {}, zgaduję: {}".format(counter, guess))
+        print("List of words {}".format(answer_list))
+        print("{} attempt, I'm guessing: {}".format(counter, guess))
         answer = input()
 
         if answer == answer_list[0]:
-            print("Wygrałem!")
+            print("I win!")
 
         elif answer == answer_list[1]:
             maks = guess
@@ -26,7 +26,7 @@ def game():
             mins = guess
 
         else:
-            print("nie oszukuj!")
+            print("Do not cheat!")
 
 
 game()
