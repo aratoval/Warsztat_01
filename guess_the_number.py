466fe639d7c23036c69157c3f05ac5397ce88090
aratoval <chy.pawel@gmail.com>
2018-10-11 11:30:17 +0200

    dodałem nowe zabezpieczenia na wprowadzane dane

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,20 +5,26 @@ from random import randint
 
 def game():
 
-    guess = randint(1, 100)
+    mins = 1
+    maxs = 100
+    guess = randint(mins, maxs)
     answer = 0
 
     while answer != guess:
 
         try:
-            answer = input("Zgadnij liczbę: ")
-            answer = int(str(answer))  # zabezpieczenie przed liczbami rzeczywistymi
-            if answer == guess:
-                print("Zgadłeś!")
-            elif answer > guess:
-                print("Za dużo!")
-            elif answer < guess:
-                print("Za mało!")
+            answer = float(input("Zgadnij liczbę z zakresu [{}, {}]: ".format(mins, maxs)))
+#            answer = int(str(answer))  # zabezpieczenie przed liczbami rzeczywistymi
+            if (10 * answer) % 10 == 0:
+                answer = int(answer)
+                if answer == guess:
+                    print("Zgadłeś!")
+                elif answer > guess:
+                    print("Za dużo!")
+                elif answer < guess:
+                    print("Za mało!")
+            else:
+                print("Podaj liczbę naturalną z zakresu [{}, {}]".format(mins, maxs))
 
         except ValueError:
             print("To nie jest liczba.")
