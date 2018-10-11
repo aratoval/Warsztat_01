063847fd722f373cc1bd5f92eeef628ed63a4bcb
aratoval <chy.pawel@gmail.com>
2018-10-11 13:18:45 +0200

    ukończone podstawowe funkcjonalności

--- flask_computer_guess_the_number.py
+++ flask_computer_guess_the_number.py
@@ -11,12 +11,12 @@ form = """
 <h1>Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach.</h1>
 <h2>Zgaduję {}</h2>
 <form action="/" method="POST">
-        <button type="submit" name="malo">za mało</button>
-        <button type="submit" name="end">zgadłeś</button>
-        <button type="submit" name="duzo">za dużo</button>
+        <button type="submit" name="button" value="malo">za mało</button>
+        <button type="submit" name="button" value="end">zgadłeś</button>
+        <button type="submit" name="button" value="duzo">za dużo</button>
         
-        <input type="hidden" name="max" value="1000">
-        <input type="hidden" name="min" value="0">
+        <input type="hidden" name="max" value="{}">
+        <input type="hidden" name="min" value="{}">
 </form>
 </center>
 """
@@ -24,14 +24,32 @@ form = """
 
 @app.route("/", methods=["GET", "POST"])
 def game():
+
     if request.method == "GET":
-        return form.format("500")
+        mins = 1
+        maks = 1000
+        answer = int((maks - mins) / 2) + mins
+
+        return form.format(answer, mins, maks)
+
     else:
-        maks = request.form.get("max")
-        mins = request.form.get("min")
-        if request.form["button"] == "malo":
-            request.form("max")
-        return form.format(str(maks))
+        action = request.form["button"]
+        maks = int(request.form.get("max"))
+        mins = int(request.form.get("min"))
+        answer = int((maks - mins) / 2) + mins
+
+        if action == "end":
+            return "<center><h1>Wygrałem!</h1></center>"
+
+        elif action == "malo":
+            maks = answer
+            answer = int((maks - mins) / 2) + mins
+
+        elif action == "duzo":
+            mins = answer
+            answer = int((maks - mins) / 2) + mins
+
+        return form.format(answer, maks, mins)
 
 
 if __name__ == "__main__":
